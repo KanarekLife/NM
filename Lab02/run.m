6e1e5088192587ecb05986006a66e2bee1e9a999
Stanisław Nieradko <stanislaw@nieradko.com>
2024-03-10 13:55:16 +0100

    lab02/02

--- Lab02/run.m
+++ Lab02/run.m
@@ -6,6 +6,6 @@ n_max = 200;
 a = 10;
 r_max = 10;
 
-[circles, index_number] = generate_circles(a, r_max, n_max);
+[circles, index_number, circle_areas] = generate_circles(a, r_max, n_max);
 plot_circles(a, circles, index_number);
 % print -dpng zadanie1.png
