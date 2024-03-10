c2f75b7143fa5ff14552a490fe724e6be72e2435
Stanisław Nieradko <stanislaw@nieradko.com>
2024-03-10 14:26:12 +0100

    lab02/04

--- Lab02/generate_circles.m
+++ Lab02/generate_circles.m
@@ -1,13 +1,18 @@
-function [circles, index_number, circle_areas] = generate_circles(a, r_max, n_max)
+function [circles, index_number, circle_areas, rand_counts, counts_mean] = generate_circles(a, r_max, n_max)
     index_number = 193044;
-    L1 = mod(index_number, 10); % always 4
+    L1 = mod(index_number, 10);
+    L2 = floor(mod(index_number, 100) / 10);
+
     if mod(L1, 2) == 0
         circles = zeros(n_max, 3);
+        rand_counts = zeros(1, n_max);
+        counts_mean = zeros(1, n_max);
         for i = 1:size(circles, 1)
             while true
                 r = rand()*r_max;
                 x = rand()*a;
                 y = rand()*a;
+                rand_counts(i) = rand_counts(i) + 1;
 
                 if x - r < 0 || x + r > a || y - r < 0 || y + r > a
                     continue
@@ -27,40 +32,15 @@ function [circles, index_number, circle_areas] = generate_circles(a, r_max, n_ma
                 end
 
                 circles(i,:) = [x, y, r];
+                tmp = cumsum(rand_counts);
+                counts_mean(i) = tmp(i) / i;
                 break
             end
         end
     else
-        circles = zeros(3, n_max);
-        for i = 1:size(circles, 2)
-            while true
-                r = rand()*r_max;
-                x = rand()*a;
-                y = rand()*a;
-
-                if x - r < 0 || x + r > a || y - r < 0 || y + r > a
-                    continue
-                end
-                
-                is_blocked = false;
-                for j = 1:i-1
-                    current_column = circles(:,j);
-                    if sqrt((current_column(1) - x)^2 + (current_column(2) - y)^2) < r + current_column(3)
-                        is_blocked = true;
-                        break;
-                    end
-                end
-
-                if is_blocked
-                    continue
-                end
-
-                circles(:,i) = [r; x; y];
-                break
-            end
-        end
+        % TBD
     end
-    if mod(floor(mod(index_number, 100) / 10), 2) == 0
+    if mod(L2, 2) == 0
         circle_areas = zeros(1, n_max);
         ssum = 0;
         for i = 1:size(circles)
--- Lab02/run.m
+++ Lab02/run.m
@@ -6,7 +6,7 @@ n_max = 200;
 a = 10;
 r_max = 10;
 
-[circles, index_number, circle_areas] = generate_circles(a, r_max, n_max);
+[circles, index_number, circle_areas, rand_counts, counts_mean] = generate_circles(a, r_max, n_max);
 plot_circles(a, circles, index_number);
 print -dpng zadanie1.png
 
