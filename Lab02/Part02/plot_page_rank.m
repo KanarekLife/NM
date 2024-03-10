6264925cf9a4bcd82867e0097387eab4e4ed542e
Stanisław Nieradko <stanislaw@nieradko.com>
2024-03-10 14:54:07 +0100

    lab02/07

---
+++ Lab02/Part02/plot_page_rank.m
@@ -0,0 +1,6 @@
+function plot_page_rank(r)
+    bar(r)
+    title 'PageRank'
+    xlabel 'Strona'
+    ylabel 'Wskaźnik PageRank'
+end
---
+++ Lab02/Part02/run.m
@@ -0,0 +1,3 @@
+[numer_indeksu, Edges, I, B, A, b, r] = page_rank();
+plot_page_rank(r);
+print -dpng zadanie7.png
