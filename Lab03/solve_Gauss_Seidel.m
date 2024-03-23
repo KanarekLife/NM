4fa66e7b43908685d338fd37939f24275abff848
Stanisław Nieradko <stanislaw@nieradko.com>
2024-03-23 08:51:46 +0100

    Lab03/04

---
+++ Lab03/solve_Gauss_Seidel.m
@@ -0,0 +1,15 @@
+function [A,b,M,bm,x,err_norm,time,iterations,index_number] = solve_Gauss_Seidel(N)
+    index_number = 193044;
+    L1 = mod(index_number, 10);
+    [A,b] = generate_matrix(N, L1);
+
+    x = ones(N, 1);
+    tic;
+    for iterations=1:1000
+        M = -1 * ((tril(A,-1)+diag(diag(A)))\triu(A,1));
+        bm = (diag(diag(A)) + tril(A, -1))\b;
+        x = M*x + bm;
+    end
+    time = toc;
+    err_norm = norm(A*x-b);
+end
