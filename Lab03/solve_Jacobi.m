5a6a55bef08739030797f5ab23ddfab35c3decf1
Stanisław Nieradko <stanislaw@nieradko.com>
2024-03-23 08:37:40 +0100

    Lab03/03

---
+++ Lab03/solve_Jacobi.m
@@ -0,0 +1,16 @@
+function [A,b,M,bm,x,err_norm,time,iterations,index_number] = solve_Jacobi(N)
+    index_number = 193044;
+    L1 = mod(index_number, 10);
+    [A,b] = generate_matrix(N, L1);
+
+    x = ones(N, 1);
+    tic;
+    for iterations = 1:1000
+        M = -1 * (diag(diag(A))\(tril(A, -1) + triu(A, 1)));
+        % inv(A) * b == A\b
+        bm = diag(diag(A))\ b;
+        x = M * x + bm;
+    end
+    time = toc;
+    err_norm = norm(A*x-b);
+end
